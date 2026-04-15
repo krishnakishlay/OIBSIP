081bd108556301e9f4dce733bc8a4132f3332370
krishnakishlay <153068670+krishnakishlay@users.noreply.github.com>
2026-04-15 20:10:55 +0530

    Update .eslintrc.cjs to include proper React plugin configuration and remove babel-eslint parser

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: 'babel-eslint', // Remove this line if it's not in dependencies
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react'],
+  rules: {
+    // Place to specify ESLint rules.
+    // e.g. 'react/hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+  },
+};
