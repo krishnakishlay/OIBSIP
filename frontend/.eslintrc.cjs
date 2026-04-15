fa70af01200e542e07ea2d1d0afb688754605e25
Krishna Kishlay <krishnakishlay30@gmail.com>
2026-04-15 19:53:18 +0530

    Fix frontend CI + ESLint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,15 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
   parser: "@babel/eslint-parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
     requireConfigFile: false,
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   plugins: ["react"],
   rules: {
